16e3d15d197c517359b6410d89ab48b17a698172
HAFEEZ-SHAIK-POLICHARLA <shaikhhafeez2019@gmail.com>
2026-06-19 20:01:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ load_dotenv()
 inngest_client = inngest.Inngest(
     app_id="rag_app",
     logger=logging.getLogger("uvicorn"),
-    is_production= False,
     serializer=inngest.PydanticSerializer()
 )
 
